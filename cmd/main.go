f2759ee34424c9135b74a808f84c6ee8173b3091
1-Holopsicon-1 <mfnbrx@gmail.com>
2024-11-08 14:46:12 +0300

    small restructure

--- cmd/main.go
+++ cmd/main.go
@@ -11,27 +11,30 @@ import (
 )
 
 func main() {
-	srv := new(server.Server)
-	log.Println("Starting server")
-	defer log.Println("End of Program")
-	session := db.Connect()
-	mHandler := handler.Handler{DB: session}
 	migr := flag.Bool("migrate", false, fmt.Sprint("Migrating Entity"))
 	rnddata := flag.Bool("rnddata", false, fmt.Sprint("Adding random data"))
 	start := flag.Bool("start", false, fmt.Sprint("Starting server"))
 	flag.Parse()
+
 	if *migr {
+		session := db.Connect()
 		log.Println("Migrating")
 		db.Migrate(session)
 	}
 	if *rnddata {
+		session := db.Connect()
 		log.Println("Creating Random Data")
 		utils.Generate(session)
 
 	}
 	if *start {
+		srv := new(server.Server)
+		log.Println("Starting server")
+		defer log.Println("End of Program")
 		log.Println("Open the server")
 		fmt.Println("Running and Serving on: http://127.0.0.1:5000")
+		session := db.Connect()
+		mHandler := handler.Handler{DB: session}
 		if err := srv.Run(":5000", mHandler.InitRoutes()); err != nil {
 			log.Fatalln(err)
 		}
